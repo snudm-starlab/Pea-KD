82e41242589d6f59046a5c7f7b24535a55d4c06a
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-24 17:51:29 +0900

    Update finetune_distilbert.py

--- src/finetune_distilbert.py
+++ src/finetune_distilbert.py
@@ -335,10 +335,7 @@ if args.do_train:
 #########################################################################
 # Model Training
 #########################################################################
-# output_model_file = '{}_nlayer.{}_lr.{}_T.{}.alpha.{}_beta.{}_bs.{}'.format(args.task_name, args.student_hidden_layers,
-#                                                                             args.learning_rate,
-#                                                                             args.T, args.alpha, args.beta,
-#                                                                             args.train_batch_size * args.gradient_accumulation_steps)
+
 if args.do_train:
     global_step = 0
     nb_tr_steps = 0
