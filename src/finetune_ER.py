8ef8427ef05490d33bce40795007bd8dc0f83548
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-24 17:50:08 +0900

    Update finetune_ER.py

--- src/finetune_ER.py
+++ src/finetune_ER.py
@@ -405,12 +405,6 @@ if args.do_train:
                                                                       args.warmup_proportion)
                     for param_group in optimizer.param_groups:
                         param_group['lr'] = lr_this_step
-                
-#                 else:
-#                     lr_this_step = args.learning_rate * warmup_linear(global_step / num_train_optimization_steps, args.warmup_proportion)
-                    
-#                     for param_group in optimizer.param_groups:
-#                         param_group['lr'] = lr_this_step
                         
                 if (global_step+1) % 50 ==0:
                     print()
