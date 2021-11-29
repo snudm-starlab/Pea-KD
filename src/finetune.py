351a636395c4b8eb719ed71867f139333c2b952e
ant6si <50072144+ant6si@users.noreply.github.com>
2021-11-29 14:46:58 +0900

    Update finetune.py

--- src/finetune.py
+++ src/finetune.py
@@ -200,11 +200,10 @@ if args.do_train:
     logger.info("  Num examples = %d", len(eval_examples))
     logger.info("  Batch size = %d", args.eval_batch_size)
 
-# if args.do_eval:
-
 #########################################################################
 # Prepare model
 #########################################################################
+
 student_config = BertConfig(os.path.join(args.bert_model, 'bert_config.json'))
 if args.kd_model.lower() in ['kd', 'kd.cls', 'kd.u', 'kd.i']:
     logger.info('using normal Knowledge Distillation')
